395f46a9d8e9676ccc38fe41fbd85777c71ab015
tulenchik <54470187+tullenchik@users.noreply.github.com>
2022-05-03 15:06:46 +0300

    Add files via upload

--- dz5/dz5/ff.cpp
+++ dz5/dz5/ff.cpp
@@ -1,29 +1,24 @@
 #include"ff.h"
 //первая часть задания
 void menu_output1() {
-	setlocale(0, "");
 	cout << "\nМеню:\n1)Создать новый массив и удалить предыдущий\n2)Зполнить ячейки случайными событиями\n3)Показать данные в консоли\n4)Выйти из программы\n";
 }
 int retrun_kol1() {
-	setlocale(0, "");
 	int n = 0;
 	cout << "\nВведите количество ячеек в массиве: ";
 	cin >> n;
 	return n;
 }
 void delete_memory1(double* mass, int n) {
-		setlocale(0, "");
 		delete []mass;
 		cout << "\nМассив успешно удален";
 }
 double* create_massiv1(int n) {
-		setlocale(0, "");
 		cout << "\nМассив успешно создан";
 		return new double[n];
 }
 void random_value1(double* mass, int n,string choice) {
 	if (choice == "2") {
-		setlocale(0, "");
 		srand(time(NULL));
 		for (int i = 0; i < n; i++) {
 			mass[i] = rand() % 100 - 50;
@@ -33,7 +28,6 @@ void random_value1(double* mass, int n,string choice) {
 }
 void output_to_the_console1(double* mass, int n,string choice) {
 	if (choice == "3") {
-		setlocale(0, "");
 		for (int i = 0; i < n; i++) {
 			cout << mass[i] << " ";
 		}
@@ -57,19 +51,16 @@ void task2(int* mass) {
 	}
 }
 void delete_memory_int2(int* mass) {
-	setlocale(0, "");
 	delete []mass;
 	cout << "\nПереход к третьему пункту задачи";
 }
 int array_size_lines3() {
-	setlocale(0, "");
 	int n = 0;
 	cout << "\nВведите количество строк: ";
 	cin >> n;
 	return n;
 }
 int array_size_columns3() {
-	setlocale(0, "");
 	int s = 0;
 	cout << "\nВведите количество столбцов: ";
 	cin >> s;
@@ -102,7 +93,6 @@ void output_double_massiv3(double** massiv, int n, int p) {
 	}
 }
 void delete_memory_task3(double** mass,int n,int p) {
-	setlocale(0, "");
 	for(int j = 0; j < n; j++) {
 		delete[] mass[j];
 	}
