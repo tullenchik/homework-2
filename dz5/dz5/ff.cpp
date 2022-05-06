2fbb86de169b396fc2563827985fc5e08482a15a
tulenchik <54470187+tullenchik@users.noreply.github.com>
2022-05-06 23:41:52 +0300

    Add files via upload

--- dz5/dz5/ff.cpp
+++ dz5/dz5/ff.cpp
@@ -1,28 +1,27 @@
 #include"ff.h"
-//первая часть задания
 void menu_output1() {
-	setlocale(0, "");
+	setlocale(LC_ALL, "rus");
 	cout << "\nМеню:\n1)Создать новый массив и удалить предыдущий\n2)Зполнить ячейки случайными событиями\n3)Показать данные в консоли\n4)Выйти из программы\n";
 }
 int retrun_kol1() {
 	int n = 0;
-	setlocale(0, "");
+	setlocale(LC_ALL, "rus");
 	cout << "\nВведите количество ячеек в массиве: ";
 	cin >> n;
 	return n;
 }
 void delete_memory1(double* mass, int n) {
 		delete []mass;
-		setlocale(0, "");
+		setlocale(LC_ALL, "rus");
 		cout << "\nМассив успешно удален";
 }
 double* create_massiv1(int n) {
-	setlocale(0, "");
+	setlocale(LC_ALL, "rus");
 		cout << "\nМассив успешно создан";
 		return new double[n];
 }
 void random_value1(double* mass, int n,string choice) {
-	setlocale(0, "");
+	setlocale(LC_ALL, "rus");
 	if (choice == "2") {
 		srand(time(NULL));
 		for (int i = 0; i < n; i++) {
@@ -32,7 +31,7 @@ void random_value1(double* mass, int n,string choice) {
 	}
 }
 void output_to_the_console1(double* mass, int n,string choice) {
-	setlocale(0, "");
+	setlocale(LC_ALL, "rus");
 	if (choice == "3") {
 		for (int i = 0; i < n; i++) {
 			cout << mass[i] << " ";
@@ -58,19 +57,19 @@ void task2(int* mass) {
 }
 void delete_memory_int2(int* mass) {
 	delete []mass;
-	setlocale(0, "");
+	setlocale(LC_ALL, "rus");
 	cout << "\nПереход к третьему пункту задачи";
 }
 int array_size_lines3() {
 	int n = 0;
-	setlocale(0, "");
+	setlocale(LC_ALL, "rus");
 	cout << "\nВведите количество строк: ";
 	cin >> n;
 	return n;
 }
 int array_size_columns3() {
 	int s = 0;
-	setlocale(0, "");
+	setlocale(LC_ALL, "rus");
 	cout << "\nВведите количество столбцов: ";
 	cin >> s;
 	return s;
@@ -102,7 +101,7 @@ void output_double_massiv3(double** massiv, int n, int p) {
 	}
 }
 void delete_memory_task3(double** mass,int n,int p) {
-	setlocale(0, "");
+	setlocale(LC_ALL, "rus");
 	for(int j = 0; j < n; j++) {
 		delete[] mass[j];
 	}
